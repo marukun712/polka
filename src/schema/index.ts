8fa8d31eacc9caf0483455b37e95b417e7cd0cc9
marukun712 <marukun530@gmail.com>
2025-10-30 13:47:10 +0900

    db

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,7 +2,9 @@ import z from "zod";
 
 export const EventSchema = z.object({
 	id: z.string().describe("イベントのSHA256ハッシュ"),
+	collection: z.string().describe("イベントの種類を識別するための一意な文字列"),
 	timestamp: z.date().describe("タイムスタンプ"),
+	pk: z.string().describe("署名検証用の公開鍵"),
 	sig: z.string().describe("署名"),
 	body: z.record(z.string(), z.any()),
 });
