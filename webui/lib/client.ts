b1604dc6b19e3fee4c90c69ce8c939fb9f8c9d0a
marukun712 <marukun530@gmail.com>
2025-12-03 15:03:49 +0900

    webui view 途中まで

--- webui/lib/client.ts
+++ webui/lib/client.ts
@@ -1,106 +1,25 @@
-import { noise } from "@chainsafe/libp2p-noise";
-import { yamux } from "@chainsafe/libp2p-yamux";
-import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
-import { type HTTP, http } from "@libp2p/http";
-import { identify } from "@libp2p/identify";
-import { webRTC } from "@libp2p/webrtc";
-import { webSockets } from "@libp2p/websockets";
-import { type Multiaddr, multiaddr } from "@multiformats/multiaddr";
-import { createLibp2p, type Libp2p } from "libp2p";
+import type { Repo } from "../dist/transpiled/interfaces/polka-repository-repo.js";
+import { repo as wasm } from "../dist/transpiled/repo.js";
+import { CarSyncStore } from "./blockstore.js";
 
 export class Client {
-	private node!: Libp2p<{ http: HTTP }>;
-	private addr: Multiaddr;
+	repo: Repo;
+	bs: CarSyncStore;
 
-	constructor(addr: Multiaddr, node?: Libp2p<{ http: HTTP }>) {
-		if (node) {
-			this.node = node;
-		}
-		this.addr = addr;
+	constructor(path: string, did: string, root: string) {
+		this.repo = wasm.open(did, root);
+		this.bs = new CarSyncStore(path, []);
 	}
 
-	public static async create(relay: string, peer: string) {
-		const node = await createLibp2p({
-			transports: [webRTC(), webSockets(), circuitRelayTransport()],
-			connectionEncrypters: [noise()],
-			streamMuxers: [yamux()],
-			services: { http: http(), identify: identify() },
-			connectionGater: {
-				denyDialMultiaddr: () => false,
-			},
-		});
-
-		await node.start();
-		const ma = multiaddr(`${relay}/p2p-circuit/p2p/${peer}`);
-		try {
-			// relayに接続
-			await node.dial(multiaddr(relay));
-			// pdsに接続
-			await node.dial(ma);
-			console.log("Connections:", node.getConnections());
-		} catch (err) {
-			console.error("Failed to dial server:", err);
-			throw err;
-		}
-
-		return new Client(ma, node);
-	}
-
-	public static async createDirect(pdsAddr: string) {
-		const node = await createLibp2p({
-			transports: [webRTC(), webSockets(), circuitRelayTransport()],
-			connectionEncrypters: [noise()],
-			streamMuxers: [yamux()],
-			services: { http: http(), identify: identify() },
-			connectionGater: {
-				denyDialMultiaddr: () => false,
-			},
-		});
-
-		await node.start();
-		const ma = multiaddr(pdsAddr);
-		try {
-			// pdsに直接接続
-			await node.dial(ma);
-			console.log("Protocols:", node.getProtocols());
-		} catch (err) {
-			console.error("Failed to dial server:", err);
-			throw err;
-		}
-
-		return new Client(ma, node);
-	}
-
-	private async fetch(
-		path: string,
-		method: string,
-		body?: Record<string, unknown>,
-	) {
-		const resource = this.addr.encapsulate(
-			`/http-path/${encodeURIComponent(path.substring(1))}`,
-		);
-
-		const blob = body
-			? new Blob([JSON.stringify(body)], { type: "application/json" })
-			: undefined;
-
-		const response = await this.node.services.http.fetch(resource, {
-			method,
-			body: blob,
-		});
-		return await response.json();
-	}
-
-	// libp2p-http-fetchはqueryを捨ててしまうため、すべてPOSTで送信
 	public getRecord(rpath: string) {
-		return this.fetch("/get", "POST", { rpath });
+		return this.repo.getRecord(rpath);
 	}
 
 	public getRecords(nsid: string) {
-		return this.fetch("/records", "POST", { nsid });
+		return this.repo.getRecords(nsid);
 	}
 
 	public allRecords() {
-		return this.fetch("/all", "POST");
+		return this.repo.allRecords();
 	}
 }
--- webui/lib/crypto.ts
+++ webui/lib/crypto.ts
@@ -1,16 +1,8 @@
 import { Secp256k1Keypair } from "@atproto/crypto";
-import { secp256k1 } from "@noble/curves/secp256k1.js";
-import { bytesToHex, hexToBytes } from "@noble/hashes/utils.js";
+import { bytesToHex } from "@noble/hashes/utils.js";
 
-export const did = "did:key:zQ3shRoxb39Z5A6Eu16CyjmbTp41RrKLJGb3myyASgxk2Ty95";
-const sk = "51948cad41be471b30a280714512160dd6d5400beb24f5a496f2b5faf0c9c933";
-// 実際はnos2xなどのセキュアな場所に保存する。
-// 秘密鍵の無断使用は、罰金バッキンガムよ！
-
-export function sign(bytes: Uint8Array): Uint8Array {
-	const skBytes = hexToBytes(sk);
-	const sig = secp256k1.sign(bytes, skBytes);
-	return sig;
+export function sign(_bytes: Uint8Array): Uint8Array {
+	throw new Error("Webui is read-only");
 }
 
 export async function generate() {
