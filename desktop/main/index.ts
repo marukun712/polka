0019bf29940f0e659e62c57eefcb0cc752f27766
marukun712 <marukun530@gmail.com>
2025-12-06 22:40:47 +0900

    desktop消し

--- desktop/main/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerGitHandlers } from "./handlers/git";
-import { registerRelayHandlers } from "./handlers/relay";
-import { registerRepoHandlers } from "./handlers/repo";
-
-export function registerAllHandlers() {
-	registerGitHandlers();
-	registerRepoHandlers();
-	registerRelayHandlers();
-}
