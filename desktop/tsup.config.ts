1d578c31c192cd78fa6834c48d16be7e90fccbf2
marukun712 <marukun530@gmail.com>
2025-12-10 16:12:34 +0900

    desktop 消し

--- desktop/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-	entry: ["main/**/*.ts", "renderer/**/*.ts", "preload/**/*.ts"],
-	format: ["esm"],
-	target: "esnext",
-	dts: true,
-});
