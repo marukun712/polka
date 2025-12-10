fbb9dfa74cc5cffb9e8511d3a307f4560bb937ac
marukun712 <marukun530@gmail.com>
2025-12-10 15:10:27 +0900

    guiが出るように

--- desktop/main/index.ts
+++ desktop/main/index.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from "node:url";
-import { electronApp, is, optimizer } from "@electron-toolkit/utils";
+import { electronApp } from "@electron-toolkit/utils";
 import { app, BrowserWindow, ipcMain, shell } from "electron";
 import { generateCommitMessage } from "./lib/git";
 import {
@@ -12,14 +12,13 @@ import {
 } from "./lib/repository";
 
 function createWindow(): void {
-	// Create the browser window.
 	const mainWindow = new BrowserWindow({
 		width: 900,
 		height: 670,
 		show: false,
 		autoHideMenuBar: true,
 		webPreferences: {
-			preload: fileURLToPath(new URL("../preload/index.mjs", import.meta.url)),
+			preload: fileURLToPath(new URL("../preload/index.js", import.meta.url)),
 			sandbox: false,
 		},
 	});
@@ -33,15 +32,7 @@ function createWindow(): void {
 		return { action: "deny" };
 	});
 
-	// HMR for renderer base on electron-vite cli.
-	// Load the remote URL for development or the local html file for production.
-	if (is.dev && process.env.ELECTRON_RENDERER_URL) {
-		mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
-	} else {
-		mainWindow.loadFile(
-			fileURLToPath(new URL("../index.html", import.meta.url)),
-		);
-	}
+	mainWindow.loadFile(fileURLToPath(new URL("../index.html", import.meta.url)));
 }
 
 // This method will be called when Electron has finished
@@ -50,13 +41,6 @@ function createWindow(): void {
 app.whenReady().then(() => {
 	// Set app user model id for windows
 	electronApp.setAppUserModelId("com.electron");
-
-	// Default open or close DevTools by F12 in development
-	// and ignore CommandOrControl + R in production.
-	// see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-	app.on("browser-window-created", (_, window) => {
-		optimizer.watchWindowShortcuts(window);
-	});
 
 	// Repository IPC handlers
 	ipcMain.handle(
