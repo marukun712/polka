89dd6dfc3ef34bac1c5d1cbaae2ac94646a99d8a
marukun712 <marukun530@gmail.com>
2025-12-12 09:40:39 +0900

    cliを調整

--- cli/index.ts
+++ cli/index.ts
@@ -1,16 +1,24 @@
+import { existsSync, mkdirSync, readFileSync } from "node:fs";
+import { homedir } from "node:os";
+import { join } from "node:path";
 import { now } from "@atcute/tid";
+import { WASIShim } from "@bytecodealliance/preview2-shim/instantiation";
+import { secp256k1 } from "@noble/curves/secp256k1.js";
+import { hexToBytes } from "@noble/hashes/utils.js";
 import { config } from "dotenv";
 import Enquirer from "enquirer";
 import keytar from "keytar";
 import { CID } from "multiformats";
-import { init } from "./daemon.ts";
 import type { GetResult } from "./dist/transpiled/interfaces/polka-repository-repo";
+import { instantiate } from "./dist/transpiled/repo.js";
+import { CarSyncStore } from "./lib/blockstore.ts";
 import { generate } from "./lib/crypto.ts";
 import {
 	cloneRepository,
 	commitAndPush,
 	existsRepository,
 	generateCommitMessage,
+	POLKA_CAR_PATH,
 	POLKA_REPO_PATH,
 	pullRepository,
 } from "./lib/git.ts";
@@ -222,6 +230,62 @@ async function main() {
 	} catch (e) {
 		console.log(e);
 		process.exit(1);
+	}
+}
+
+// repoをinitする
+export async function init(sk: string, didKey: string) {
+	// ~/.polkaがあるか確認
+	if (!existsSync(join(homedir(), ".polka"))) {
+		mkdirSync(join(homedir(), ".polka"));
+	}
+
+	const path = POLKA_CAR_PATH;
+	const store = new CarSyncStore(path);
+
+	// WASMのロード
+	const loader = async (path: string) => {
+		const buf = readFileSync(`./dist/transpiled/${path}`);
+		return await WebAssembly.compile(new Uint8Array(buf));
+	};
+
+	// importする関数をバインド
+	const wasm = await instantiate(loader, {
+		//@ts-expect-error
+		"polka:repository/crypto": {
+			sign: (bytes: Uint8Array) => {
+				const skBytes = hexToBytes(sk);
+				const sig = secp256k1.sign(bytes, skBytes);
+				return sig;
+			},
+		},
+		"polka:repository/blockstore": {
+			readBlock: (cid: Uint8Array) => {
+				const parsed = CID.decode(cid);
+				const out: Uint8Array[] = [];
+				store.readBlock(parsed, out);
+				if (!out[0]) throw new Error("Block not found.");
+				return out[0];
+			},
+			writeBlock: (codec: bigint, hash: bigint, contents: Uint8Array) => {
+				return store.writeBlock(Number(codec), Number(hash), contents);
+			},
+		},
+		...new WASIShim().getImportObject<"0.2.6">(),
+	});
+
+	// repoを開く
+	if (existsSync(path)) {
+		store.updateIndex();
+		const roots = store.getRoots();
+		if (!roots[0]) throw new Error("Root not found.");
+		return { repo: wasm.repo.open(didKey, roots[0].toString()), store };
+	} else {
+		store.create();
+		const repo = wasm.repo.create(didKey);
+		const root = repo.getRoot();
+		store.updateHeaderRoots([CID.parse(root)]);
+		return { repo, store };
 	}
 }
 
