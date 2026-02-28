4387a3187283e485a294f5aa81ee38b793a19749
marukun712 <marukun530@gmail.com>
2026-02-28 10:20:28 +0900

    simple flake

--- flake.nix
+++ flake.nix
@@ -35,28 +35,12 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs_24
-            pkgs.pnpm
             pkgs.bun
-            pkgs.wit-bindgen
             pkgs.mdbook
-
-            pkgs.pkg-config
-            pkgs.openssl
-
             pkgs.electron
-            pkgs.glib
-            pkgs.glib.dev
-            pkgs.libsecret
-            pkgs.libsecret.dev
-            pkgs.gnome-keyring
-
             rust
           ];
           shellHook = ''
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.libsecret.dev}/lib/pkgconfig:${pkgs.glib.out}/lib/pkgconfig"
-            export LD_LIBRARY_PATH="${pkgs.libsecret.out}/lib:${pkgs.glib.out}/lib"
-
             export ELECTRON_EXEC_PATH="${pkgs.electron}/bin/electron"
             export ELECTRON_SKIP_BINARY_DOWNLOAD="1"
           '';
